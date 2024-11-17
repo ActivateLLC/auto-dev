3a9c8b6a6a051fe38965b5b5559f9cd5de0d5bc1
Aaron Aldana <usualprovider@gmail.com>
2024-11-17 15:15:07 -0800

    chore: Update dependencies and add new types for sinon and mocha

--- creai/src/test/suite/index.ts
+++ creai/src/test/suite/index.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import { glob } from 'glob';
+import * as globModule from 'glob';
+const { glob } = globModule;
 
 export function run(): Promise<void> {
     // Create the mocha test
@@ -11,27 +12,23 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, '..');
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err: Error | null, files: string[]) => {
-            if (err) {
-                return e(err);
-            }
-
+    return new Promise((resolve, reject) => {
+        glob('**/**.test.js', { cwd: testsRoot }).then(files => {
             // Add files to the test suite
-            files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
             try {
                 // Run the mocha test
                 mocha.run(failures => {
                     if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
+                        reject(new Error(`${failures} tests failed.`));
                     } else {
-                        c();
+                        resolve();
                     }
                 });
             } catch (err) {
-                e(err);
+                reject(err);
             }
-        });
+        }).catch(err => reject(err));
     });
 } 
