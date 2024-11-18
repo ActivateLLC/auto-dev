fd563a1379c133b4a7be275aa24490f4c833c518
Aaron Aldana <usualprovider@gmail.com>
2024-11-17 16:10:18 -0800

    refactor: Update Mocha configuration to use async/await in test suite runner

--- creai/src/test/suite/index.ts
+++ creai/src/test/suite/index.ts
@@ -1,8 +1,9 @@
 import * as path from 'path';
 import Mocha = require('mocha');
-import * as globModule from 'glob';
+import { glob } from 'glob';
+import { promisify } from 'util';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
@@ -12,16 +13,16 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, '..');
 
-    return new Promise((resolve, reject) => {
-        // Use promisify for glob
-        globModule.glob('**/**.test.js', { cwd: testsRoot }, (err: Error | null, files: string[]) => {
-            if (err) {
-                return reject(err);
-            }
+    try {
+        // Use glob with promises
+        const files = await glob('**/**.test.js', { 
+            cwd: testsRoot 
+        });
 
-            // Add files to the test suite
-            files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+        // Add files to the test suite
+        files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
 
+        return new Promise((resolve, reject) => {
             try {
                 // Run the mocha test
                 mocha.run((failures: number): void => {
@@ -35,5 +36,8 @@ export function run(): Promise<void> {
                 reject(err);
             }
         });
-    });
+    } catch (err) {
+        console.error('Error during test setup:', err);
+        throw err;
+    }
 } 
